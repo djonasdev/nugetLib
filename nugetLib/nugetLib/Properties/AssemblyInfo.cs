c318a6a883dfeb85b6cdb13c27769542a643fb62
Dominic Jonas <djonas@haprotec.de>
2017-11-06 10:05:43 +0100

    heat cmd angepasst

--- nugetLib/nugetLib/Properties/AssemblyInfo.cs
+++ nugetLib/nugetLib/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.1.2")]
-[assembly: AssemblyFileVersion("0.4.1.2")]
-[assembly: AssemblyInformationalVersion("0.4.1-2+85b8f3c")]
+[assembly: AssemblyVersion("0.4.1.3")]
+[assembly: AssemblyFileVersion("0.4.1.3")]
+[assembly: AssemblyInformationalVersion("0.4.1-3+00df19c")]
